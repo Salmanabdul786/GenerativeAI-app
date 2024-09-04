49f3d065f0f60cb5db37e53bf5d9c97be6443d1c
Abdul Kareem <41697424+Salmanabdul786@users.noreply.github.com>
2024-09-04 22:54:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -154,12 +154,11 @@ app.get("/", (req, res) => {
 });
 
 
-app.use(express.static(path.join(__dirname, "../frontend")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
-
 
 
 
