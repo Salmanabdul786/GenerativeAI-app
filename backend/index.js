eba792ea805dbe8360978549f381c90e3ec8f789
Abdul Kareem <41697424+Salmanabdul786@users.noreply.github.com>
2024-09-08 02:45:18 +0530

    up

--- backend/index.js
+++ backend/index.js
@@ -149,16 +149,8 @@ app.use((err, req, res, next) => {
 
 app.use(express.static(path.join(__dirname,"../frontend/dist")));
 app.get("*",(req,res)=>{
-res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
 });
-
-
-app.use(express.static(path.join(__dirname,"../frontend/dist")));
-app.get("*",(req,res)=>{
-res.sendFile(path.join(__dirname,"../frontend/dist","index.html"));
-});
-
-
 
 
 app.listen(port, () => {
